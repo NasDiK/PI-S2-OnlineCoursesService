4b03b0116a0a2f4595802733fbdcebe9d2cdc9b0
a.s.tungusov <tungusov-aleksandr.fl@yandex.ru>
2023-02-24 23:02:43 +0500

    added knex

---
+++ services/api/knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'postgres',
+      user:     'postgres',
+      password: 'root'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: 'migrations'
+    }
+  }
+};
